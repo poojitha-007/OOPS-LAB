40db613f68f680677681532450930b6959e1f567
poojitha-007 <poojitha.tpt1@gmail.com>
2025-12-23 09:35:59 +0530

    Add files via upload

---
+++ Lab-1/GreatestNumber.java
@@ -0,0 +1,12 @@
+class GreatestNumber {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        if (a > b) {
+            System.out.println("A is greater");
+        } else {
+            System.out.println("B is greater");
+        }
+    }
+}
