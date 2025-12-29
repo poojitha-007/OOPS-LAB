9f98771bbb4a0fe3f955fab6df0b256dc28acd1c
poojitha-007 <poojitha.tpt1@gmail.com>
2025-12-29 20:59:54 +0530

    Add files via upload

---
+++ Lab-1/DataTypes.java
@@ -0,0 +1,19 @@
+class DataTypes{	
+public static void main(String[] args)
+{
+ int a= 20;
+ byte b = 87;
+ float c = 5.87f;
+ double d = 78.2d;
+ char grade = 'A';
+ boolean isStudent = true;
+
+System.out.println("a:"+ a);
+System.out.println("b:"+ b);
+System.out.println("c:"+ c);
+System.out.println("d:"+ d);
+System.out.println("grade:"+ grade);
+System.out.println("isStudent:"+ isStudent);
+}
+}
+
