bc3fdce53c0f7c6a667bf0b9ff69bfb6a956c272
poojitha-007 <poojitha.tpt1@gmail.com>
2026-04-12 20:25:03 +0530

    Add files via upload

---
+++ Encapsulation/Bank.java
@@ -0,0 +1,28 @@
+class BankAccount {
+    private double balance;
+
+    // setter with validation
+    public void setBalance(double balance) {
+        if (balance >= 0) {
+            this.balance = balance;
+        } else {
+            System.out.println("Invalid balance!");
+        }
+    }
+
+    // getter
+    public double getBalance() {
+        return balance;
+    }
+}
+
+public class  Bank {
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount();
+
+        acc.setBalance(5000);
+        System.out.println("Balance: " + acc.getBalance());
+
+        acc.setBalance(-100);   // invalid
+    }
+}
